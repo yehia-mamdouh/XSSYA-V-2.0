9abc8c6e9884fdf7005c91f41cd2d95dbf84d266
yehia-mamdouh <yehiamamdouh51@hotmail.com>
2015-03-09 16:52:02 +0400

    Create cve.py

---
+++ cve.py
@@ -0,0 +1,442 @@
+#!/usr/bin/env python
+# -*- coding: cp1252 -*-
+# XSSA is a Cross Site Scripting Scanner & Vulnerability Confirmation
+# By Yehia Mamdouh - twitter.com/@Yehia1mamdouh / Facebook/yehia.mamdouh.98
+
+
+import colorama
+from colorama import init
+from colorama import Fore, Back, Style
+colorama.init()
+
+def Apache():
+    print Fore.GREEN + "CVE-2013-2187"
+    print Fore.CYAN + "Apache : Archiva 1.3 to Archiva 1.3.6"
+    print "http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2013-2187"
+    print ""
+
+    print Fore.GREEN + "CVE-2011-1077"
+    print Fore.CYAN +  "Apache : Archiva 1.3 to 1.3.4"
+    print "info : http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2011-1077"
+    print ""
+
+    print Fore.GREEN + "CVE-2011-0533"
+    print Fore.CYAN + "Apache Archiva 1.3 to 1.3.3"
+    print "info : http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2011-0533"
+    print ""
+
+    
+    print Fore.GREEN + "CVE-2014-0232"
+    print Fore.CYAN + "Apache OFBiz 11.04.01 to 11.04.04 | 12.04.01 to 11.04.03"
+    print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-0232"
+    print ""
+
+    print Fore.GREEN + 'CVE-2012-4558'
+    print Fore.CYAN + 'Apache HTTP Server 2.2.x before 2.2.24-dev and 2.4.x before 2.4.4'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2012-4558'
+    print""
+
+    print Fore.GREEN + 'CVE-2013-6111'
+    print Fore.CYAN + 'Apache HTTP Server mod_pagespeed module 0.x, 1.0.22.7, 1.1.x, 1.24.1,(1.3.25.1 1.3.25.4) (1.4.26.1  1.4.26.4) (1.5.27.1  1.5.27.3)(1.6.29.1 1.6.29.6)'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2013-6111'
+    print""
+
+    print Fore.GREEN + 'CVE-2012-3499'
+    print Fore.CYAN + 'Apache HTTP Server 2.2.x before 2.2.24-dev and 2.4.x before 2.4.4'
+    print 'http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2012-3499'
+    print""
+
+    print Fore.GREEN + 'CVE-2009-5120'
+    print Fore.CYAN + 'Apache Tomcat in Websense Manager in Websense Web Security 7.0 and Web Filter 7.0'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2009-5120'
+    print""
+
+    print Fore.GREEN + 'CVE-2012-4360'
+    print Fore.CYAN + 'Apache HTTP Server mod_pagespeed module 0.10.19.1 through 0.10.22.4'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2012-4360'
+    print""
+
+    print Fore.GREEN + 'CVE-2012-2687'
+    print Fore.CYAN + 'Apache HTTP Server 2.4.x before 2.4.3 in make_variant_list function in mod_negotiation.c in the mod_negotiation module'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2012-2687'
+    print""
+
+    print Fore.GREEN + 'CVE-2011-1570'
+    print Fore.CYAN + 'Apache Tomcat 6.x before 6.0.6 GA in Liferay Portal Community Edition (CE)'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2011-1570'
+    print""
+
+    print Fore.GREEN + 'CVE-2011-0013'
+    print Fore.CYAN + 'Apache Tomcat 5.5 before 5.5.32, 6.0 before 6.0.30, and 7.0 before 7.0.6in HTML Manager Interface'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2011-0013'
+    print""
+
+    print Fore.GREEN + 'CVE-2010-4172'
+    print Fore.CYAN + 'Apache Tomcat 6.0.12 through 6.0.29 and 7.0.0 through 7.0.4 in Manager application'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2010-4172'
+    print""
+
+    print Fore.GREEN + 'CVE-2009-2696'
+    print Fore.CYAN + 'Apache Tomcat on Red Hat Enterprise Linux 5, Desktop Workstation 5, and Linux Desktop 5'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2009-2696'
+    print""
+
+    print Fore.GREEN + 'CVE-2002-0840'
+    print Fore.CYAN + 'Apache 2.0 before 2.0.43, and 1.3.x up to 1.3.26'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2002-0840'
+    print""
+
+    print Fore.GREEN + 'CVE-2006-3918'
+    print Fore.CYAN + 'Apache HTTP Server 1.3 before 1.3.35, 2.0 before 2.0.58, and 2.2 before 2.2.2'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2006-3918'
+    print""
+
+    print Fore.GREEN + 'CVE-2005-3352'
+    print Fore.CYAN + 'Apache httpd before 1.3.35-dev and Apache httpd 2.0.x before 2.0.56-dev'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2005-3352'
+    print""
+
+    print Fore.GREEN + 'CVE-2005-2088'
+    print Fore.CYAN + 'Apache HTTP server before 1.3.34, and 2.0.x before 2.0.55'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2005-2088'
+    print""
+
+    print Fore.GREEN + 'CVE-2004-0940'
+    print Fore.CYAN + 'http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2004-0940'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2004-0940'
+    print""
+
+    print Fore.GREEN + 'CVE-2002-1567'
+    print Fore.CYAN + 'Apache Tomcat 4.1'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2002-1567'
+    print""
+
+    print Fore.GREEN + 'CVE-2003-1581'
+    print Fore.CYAN + 'Apache HTTP Server 2.0.44'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2003-1581'
+    print""
+
+    print Fore.GREEN + 'CVE-2009-2823'
+    print Fore.CYAN + 'Apache HTTP Server in Apple Mac OS X before 10.6.2'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2009-2823'
+    print""
+
+    print Fore.GREEN + 'CVE-2009-0781'
+    print Fore.CYAN + 'Apache Tomcat 4.1.0 through 4.1.39, 5.5.0 through 5.5.27, and 6.0.0 through 6.0.18'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2009-0781'
+    print""
+
+    print Fore.GREEN + 'CVE-2008-1232'
+    print Fore.CYAN + 'Apache Tomcat 4.1.0 through 4.1.37, 5.5.0 through 5.5.26, and 6.0.0 through 6.0.16'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2008-1232'
+    print""
+
+    print Fore.GREEN + 'CVE-2008-1947'
+    print Fore.CYAN + 'Apache Tomcat 5.5.9 through 5.5.26 and 6.0.0 through 6.0.16'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2008-1947'
+    print""
+
+    print Fore.GREEN + 'CVE-2008-2168'
+    print Fore.CYAN + 'Apache 2.2.6'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2008-2168'
+    print""
+
+    print Fore.GREEN + 'CVE-2008-0455'
+    print Fore.CYAN + 'Apache HTTP Server 2.2.6 and earlier in the 2.2.x, 2.0.61 and earlier in the 2.0.x , and 1.3.39 and earlier in the 1.3.x '
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2008-0455'
+    print""
+
+    print Fore.GREEN + 'CVE-2008-0005'
+    print Fore.CYAN + 'Apache 2.2.x before 2.2.7-dev, 2.0.x before 2.0.62-dev, and 1.3.x before 1.3.40-dev'
+    print 'http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2008-0005'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-6421'
+    print Fore.CYAN + 'Apache HTTP Server 2.2.0 through 2.2.6'
+    print 'http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-6421'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-6388'
+    print Fore.CYAN + 'Apache HTTP Server 2.2.0 through 2.2.6, 2.0.35 through 2.0.61, and 1.3.2 through 1.3.39'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-6388'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-5000'
+    print Fore.CYAN + 'Apache HTTP Server 1.3.0 through 1.3.39 and 2.0.35 through 2.0.61'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-5000'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-6203'
+    print Fore.CYAN + 'Apache HTTP Server 2.0.x and 2.2.x'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-6203'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-4465'
+    print Fore.CYAN + 'Apache HTTP Server before 2.2.6'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-4465'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-3386'
+    print Fore.CYAN + 'Apache Tomcat 6.0.0 to 6.0.13 and 5.5.0 to 5.5.24'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-3386'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-3384'
+    print Fore.CYAN + 'Apache Tomcat 3.3 through 3.3.2'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-3384'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-3383'
+    print Fore.CYAN + 'Apache Tomcat 4.0.0 through 4.0.6 and 4.1.0 through 4.1.36'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-3383'
+    print""
+
+    print Fore.GREEN + 'CVE-2006-5752'
+    print Fore.CYAN + 'Apache HTTP Server (httpd)'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2006-5752'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-2449'
+    print Fore.CYAN + 'Apache Tomcat 4.0.0 through 4.0.6, 4.1.0 through 4.1.36, 5.0.0 through 5.0.30, 5.5.0 through 5.5.24, and 6.0.0 through 6.0.13'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-2449'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-2450'
+    print Fore.CYAN + 'Apache Tomcat 4.0.0 through 4.0.6, 4.1.0 through 4.1.36, 5.0.0 through 5.0.30 5.5.0 through 5.5.24, and 6.0.0 through 6.0.13'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-2450'
+    print""
+
+    print Fore.GREEN + 'CVE-2006-7195'
+    print Fore.CYAN + 'Apache Tomcat 5.0.0 through 5.0.30 and 5.5.0 through 5.5.17'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2006-7195'
+    print""
+
+    print Fore.GREEN + 'CVE-2006-7196'
+    print Fore.CYAN + 'Apache Tomcat 4.0.0 through 4.0.6, 4.1.0 through 4.1.31, 5.0.0 through 5.0.30, and 5.5.0 through 5.5.15'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2006-7196'
+    print""
+
+    print Fore.GREEN + 'CVE-2007-1358'
+    print Fore.CYAN + 'Apache Tomcat 4.0.0 through 4.0.6 and 4.1.0 through 4.1.34'
+    print 'info: http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2007-1358'
+
+
+
+def WordPess():
+   print Fore.GREEN + "CVE-2014-5347"
+   print Fore.CYAN + "Disqus Comment System plugin before 2.76 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-5347"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-5345"
+   print Fore.CYAN + "Disqus Comment System plugin before 2.76 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-5345"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-5344"
+   print Fore.CYAN + "Mobiloud (mobiloud-mobile-app-plugin) pluginbefore 2.3.8 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-5344"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-5240"
+   print Fore.CYAN + "wp-includes/pluggable.php when Multisite is enabled for WordPress before 3.9.2"
+   print "info: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-5240"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-5202"
+   print Fore.CYAN + "compfight-search.php in the Compfight plugin 1.4 WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-5202"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4856"
+   print Fore.CYAN + "Polldaddy Polls & Ratings plugin before 2.0.25 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4856"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4855"
+   print Fore.CYAN + "Polylang plugin before 1.5.2 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4855"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4854"
+   print Fore.CYAN + "WP Construction Mode plugin 1.8 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4854"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4847"
+   print Fore.CYAN + "Random Banner plugin 1.1.2.1  WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4847"
+   print ""
+
+
+   print Fore.GREEN + "CVE-2014-4846"
+   print Fore.CYAN + "Meta Slider (ml-slider) plugin 2.5 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4846"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4845"
+   print Fore.CYAN + "BannerMan plugin 0.2.4 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4845"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4724"
+   print Fore.CYAN + "Custom Banners plugin 1.2.2.2 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4724"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4723"
+   print Fore.CYAN + "Easy Banners plugin 1.4 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4723"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4717"
+   print Fore.CYAN + "Simple Share Buttons Adder plugin before 4.5 for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4717"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4595"
+   print Fore.CYAN + "WP RESTful plugin 0.1 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4594"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4594"
+   print Fore.CYAN + "index.php WordPress Responsive Preview plugin 1.2"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4594"
+   print ""
+
+
+   print Fore.GREEN + "CVE-2014-4593"
+   print Fore.CYAN + "wp-plugins-net/index.php in the WP Plugin Manager (wppm) plugin 1.6.4.b and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4593"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4591"
+   print Fore.CYAN + "picasa_upload.php in the WP-Picasa-Image plugin 1.0 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4591"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4590"
+   print Fore.CYAN + "get.php in the WP Microblogs plugin 0.4.0 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4590"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4587"
+   print Fore.CYAN + "WP GuestMap plugin 1.8 and earlier for WordPress"
+   print "info: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4587"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4585"
+   print Fore.CYAN + "WP-FaceThumb plugin 1.0 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4585"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4584"
+   print Fore.CYAN + "admin/editFacility.php in the wp-easybooking plugin 1.0.3 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4584"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4583"
+   print Fore.CYAN + "forms/messages.php in the WP-Contact (wp-contact-sidebar-widget) plugin 1.0 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4583"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4582"
+   print Fore.CYAN + "admin/admin_show_dialogs.php in the WP Consultant plugin 1.0 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4582"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4581"
+   print Fore.CYAN + "facture.php in the WPCB plugin 2.4.8 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4581"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4580"
+   print Fore.CYAN + "blipbot.ajax.php in the WP BlipBot plugin 3.0.9 and earlier WordPress"
+   print "info: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4580"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4579"
+   print Fore.CYAN + "js/test.php in the Appointments Scheduler plugin 1.5 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4579"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4578"
+   print Fore.CYAN + "asset-studio/icons-launcher.php in the WP App Maker plugin 1.0.16.4 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4578"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4576"
+   print Fore.CYAN + "services/diagnostics.php in the WordPress Social Login plugin 2.0.3 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4576"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4575"
+   print Fore.CYAN + "js/window.php in the Wikipop plugin 2.0 and earlier for WordPress"
+   print "info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4575"
+   print ""
+
+   print Fore.GREEN + "CVE-2014-4574"
+   print Fore.CYAN + "resize.php in the WebEngage plugin for WordPress before 2.0.1"
+   print 'info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4574'
+   print ""
+
+   print Fore.GREEN + 'CVE-2014-4573'
+   print Fore.CYAN + 'frame-maker.php in the Walk Score plugin 0.5.5 and earlier for WordPress'
+   print 'info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4573'
+   print ""
+
+   print Fore.GREEN + 'CVE-2014-4571'
+   print Fore.CYAN + 'vncal.js.php in the VN-Calendar plugin 1.0 and earlier for WordPress'
+   print 'info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4571'
+   print ""
+
+   print Fore.GREEN + 'CVE-2014-4570'
+   print Fore.CYAN + 'VideoWhisper Video Presentation plugin for WordPress before 3.31'
+   print 'info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4570'
+   print ""
+
+   print Fore.GREEN + 'CVE-2014-4569'
+   print Fore.CYAN + 'ls/vv_login.php in the VideoWhisper Live Streaming Integration plugin 4.27.2 and earlier for WordPress'
+   print 'info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4569'
+   print ""
+
+   print Fore.GREEN + 'CVE-2014-4568'
+   print Fore.CYAN + 'posts/videowhisper/r_logout.php 1.55.4 and earlier for WordPress'
+   print 'info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4568'
+   print ""
+
+   print Fore.GREEN + 'CVE-2014-4565'
+   print Fore.CYAN + 'vcc.js.php in the Verification Code for Comments plugin 2.1.0 and earlier for WordPress'
+   print 'info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4565'
+   print ""
+
+   print Fore.GREEN +' CVE-2014-4564'
+   print Fore.CYAN + 'check.php in the Validated plugin 1.0.2 and earlier for WordPress'
+   print 'info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4564'
+   print ""
+
+   print Fore.GREEN + 'CVE-2014-4563'
+   print Fore.CYAN + 'go.php in the URL Cloak & Encrypt (url-cloak-encrypt) plugin 2.0 and earlier for WordPress'
+   print 'info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4563'
+   print ""
+
+def PHPmyAdmin():
+    print Fore.GREEN + 'CVE-2014-5274'
+    print Fore.CYAN + 'phpMyAdmin 4.1.x before 4.1.14.3 and 4.2.x before 4.2.7.1'
+    print 'info: http://www.cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-5274'
+    print ""
+
+    print Fore.GREEN + 'CVE-2014-5273'
+    print Fore.CYAN + 'phpMyAdmin 4.0.x before 4.0.10.2, 4.1.x before 4.1.14.3, and 4.2.x before 4.2.7.1'
+    print 'info: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-5273'
+    print ""
+
+    print Fore.GREEN + 'CVE-2014-4986'
+    print Fore.CYAN + 'phpMyAdmin 4.0.x before 4.0.10.1, 4.1.x before 4.1.14.2, and 4.2.x before 4.2.6'
+    print 'info: http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-4986'
+    print ""
+
+
+
+
+
